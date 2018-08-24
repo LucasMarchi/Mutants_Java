965355c1a69642dc2da35dd1dad89919b8c0eb6c
Lucas Marchi <lucas-marchi@hotmail.com>
2018-08-24 11:21:41 -0300

    Corrigindo import

--- src/main/java/com/irmandade/mutants/services/HumanoService.java
+++ src/main/java/com/irmandade/mutants/services/HumanoService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.irmandade.mutants.HumanoRepository;
+import com.irmandade.mutants.repositories.HumanoRepository;
 import com.irmandade.mutants.models.Humano;
 
 @Service
