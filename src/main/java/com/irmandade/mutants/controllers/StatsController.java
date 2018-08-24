3158b921d4602edc7f8e017afc72f780e8108da1
Lucas Marchi <lucas-marchi@hotmail.com>
2018-08-24 11:19:28 -0300

    Corrigindo import

--- src/main/java/com/irmandade/mutants/controllers/StatsController.java
+++ src/main/java/com/irmandade/mutants/controllers/StatsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.irmandade.mutants.HumanoRepository;
+import com.irmandade.mutants.repositories.HumanoRepository;
 import com.irmandade.mutants.services.StatsService;
 
 import io.swagger.annotations.ApiResponse;
